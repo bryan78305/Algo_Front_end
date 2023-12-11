1ff585b4834ebfaa4048d446270bb56eafc66b73
Spicer Matthews <github@cloudmanic.com>
2023-12-10 20:34:07 -0800

    github pages config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   output: "export",
+  assetPrefix: !debug ? "/crypto-algo-frontend/" : "",
 };
 
 module.exports = nextConfig;
